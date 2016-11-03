337a2585abd0d2d15f9496012bca53c30d05ebeb
Akshar Joshi <akshar.joshi91@gmail.com>
2016-11-02 18:06:07 -0700

    API for expiration analysis

--- nodejs/routes/visualization.js
+++ nodejs/routes/visualization.js
@@ -26,5 +26,23 @@ exports.seasonalAnalysis = function(req, res){
 	});
 };
 
+exports.expirartionAnalysis = function(req, res){
+    var condition = {};
+    
+    if(req.query){
+        condition = req.query;
+    };
+
+    var qs = "SELECT Year(`EXPIRATION DATE`) AS Year, QUARTER(`FILING DATE`) As Quarter,`PERMIT TYPE`,COUNT(*) As Count FROM `permit_history` WHERE Year(`FILING DATE`)>=2012 GROUP BY Year(`FILING DATE`),QUARTER(`FILING DATE`),`PERMIT TYPE`";
+
+    dbObject.find(qs/*condition, '*' , {}, 0, 0, {}*/, function(err, response){
+        if (err) {
+            //console.log("err", err);
+            res.status(500).jsonp(err)
+        }
+        console.log("\n\nresponse FOR seasonalAnalysis is: ", response);
+        res.jsonp(response);
+    });
+};
 
 //SELECT Year(`FILING DATE`), QUARTER(`FILING DATE`),`PERMIT TYPE`,COUNT(*) FROM `permit_history` WHERE Year(`FILING DATE`)>=2012 GROUP BY Year(`FILING DATE`),QUARTER(`FILING DATE`),`PERMIT TYPE`;
