515898cb441721b7090e6643d6e8aa1e5654f26e
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-03 02:58:11 -0700

    visualization changes

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -201,12 +201,12 @@ app.controller('analyticsController', function($scope,$http) {
 					console.log(zipObj[obj.zipcode][obj.Permit_Type]);
 					if(typeof zipObj[obj.zipcode][obj.Permit_Type] == "undefined" || typeof zipObj[obj.Permit_Type][obj.Permit_Type] == null){
 						console.log(">>>>>>>>>>>> 2 <<<<<<<<<<<<");
-						zipObj[obj.zipcode][obj.Permit_Type] = {}; 
-						zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+						//zipObj[obj.zipcode][obj.Permit_Type] = {}; 
+						//zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
 					}
 					else{
 						console.log(">>>>>>>>>>>> 3 <<<<<<<<<<<<");
-						zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+						//zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
 					}	
 				}
 			}
