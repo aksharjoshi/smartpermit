a2adcc3949dcf3541eca8ded574bd9182a39efd5
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-02 22:55:04 -0700

    visualization changes

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -74,8 +74,6 @@ app.controller('homeController', function($scope,$http) {
 });
 app.controller('analyticsController', function($scope,$http) {
 	
-	//$scope.CommunityListObj = {};
-	
 	$http.get("/seasonalAnalysis?year=2012").success(function(response){
 		var seosonalTrendArray = [];
 		var permitTypeCountArray = [];
@@ -93,27 +91,12 @@ app.controller('analyticsController', function($scope,$http) {
 
 		$(permitTypes).each(function(idx,permit_type){
 			counts = [];
-			console.log(permitTypeCountArray[permit_type]);
-			for(var i=0; i<4; i++)
+			for(var i=0; i<4; i++){
 				counts.push(permitTypeCountArray[permit_type][i]);
-			console.log(counts);
+			}
 			seosonalTrendArray.push({name: permit_type, data: counts})
 		});
-		var series = [{
-		            name: 'PM',
-		            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-		        }, {
-		            name: 'New York',
-		            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-		        }, {
-		            name: 'Berlin',
-		            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-		        }, {
-		            name: 'London',
-		            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-		        }];
-		console.log(series);
-		console.log(seosonalTrendArray);
+		
 		$(function () {
 		    Highcharts.chart('containerSeasonalAnalytics', {
 		        title: {
@@ -129,7 +112,7 @@ app.controller('analyticsController', function($scope,$http) {
 		        },
 		        yAxis: {
 		            title: {
-		                text: 'Temperature (°C)'
+		                text: 'Total Number of Permits'
 		            },
 		            plotLines: [{
 		                value: 0,
