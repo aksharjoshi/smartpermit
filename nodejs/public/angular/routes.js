2bdab490232d8cc617875a78eb51de19ec151e2e
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-02 20:19:41 -0700

    visualization changes

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -87,6 +87,9 @@ app.controller('analyticsController', function($scope,$http) {
 			permitTypeCountArray[obj.Permit_Type][parseInt(obj.Quarter)-1] = obj.Count;
 		});
 		console.log(permitTypeCountArray);
+		$(permitTypeCountArray).each(function(permit_type,obj){
+			console.log(permit_type);
+		});
 		var series = [{
 		            name: 'PM',
 		            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
