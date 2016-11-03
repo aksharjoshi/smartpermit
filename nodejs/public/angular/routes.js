a532cf90ce39b8ce68692596bdff1d1959330bc2
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-03 00:28:59 -0700

    visualization changes

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -197,7 +197,8 @@ app.controller('analyticsController', function($scope,$http) {
 			}
 			else{
 				console.log("*** 1 ***"+obj.zipcode);
-				if(zipObj[obj.zipcode] != "undefined" && zipObj[obj.zipcode] != null && zipObj[obj.zipcode] != ""){
+				if(obj.zipcode != "undefined" && obj.zipcode != null && obj.zipcode != ""){
+					console.log("*** 4 ***"+);
 					if(typeof zipObj[obj.zipcode][obj.Permit_Type] == "undefined" || typeof zipObj[obj.Permit_Type][obj.Permit_Type] == null){
 						console.log("*** 3 ***");
 						zipObj[obj.zipcode][obj.Permit_Type] = []; 
