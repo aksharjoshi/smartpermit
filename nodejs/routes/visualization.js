d6e751e53910faf88dfa06fe020315984cfb4dc4
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-02 23:47:36 -0700

    visualization changes

--- nodejs/routes/visualization.js
+++ nodejs/routes/visualization.js
@@ -60,7 +60,7 @@ exports.heatMap = function(req, res){
     var inputYear = req.query.year;
 
    // var qs = "SELECT Year(`FILING DATE`) As Year, QUARTER(`FILING DATE`) As Quarter,`BOROUGH`,`ZIP CODE`,`PERMIT TYPE`,COUNT(*) FROM `permit_history` WHERE Year(`FILING DATE`)="+ inputYear + " AND `RESIDETIAL` = 'YES' GROUP BY Year(`FILING DATE`),QUARTER(`FILING DATE`),`BOROUGH`,`ZIP CODE`,`PERMIT TYPE`";
-    var qs = "SELECT `BOROUGH`,`ZIP CODE` As zipcode,`PERMIT TYPE`,COUNT(*) FROM `permit_history` WHERE `RESIDETIAL` = 'YES' "
+    var qs = "SELECT `BOROUGH`,`ZIP CODE` As zipcode,`PERMIT TYPE` As Permit_Type,COUNT(*) FROM `permit_history` WHERE `RESIDETIAL` = 'YES' "
     +" GROUP BY `BOROUGH`,`ZIP CODE`,`PERMIT TYPE`";
     dbObject.find(qs/*condition, '*' , {}, 0, 0, {}*/, function(err, response){
         if (err) {
