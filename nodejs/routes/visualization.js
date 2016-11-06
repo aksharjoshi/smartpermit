56ae0838ab08c97aac7ca8bb44d4424d65da1af2
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-06 14:36:36 -0800

    Addind Year wise data selection Optiopn - MySQL Query Update

--- nodejs/routes/visualization.js
+++ nodejs/routes/visualization.js
@@ -15,8 +15,20 @@ exports.seasonalAnalysis = function(req, res){
     };
 
     var inputYear = req.query.year;
+    if(inputYear != "undefined" && inputYear != null && inputYear != "")
+        var inputYearStr = " AND Year(`FILING DATE`)=" + inputYear;
+    else
+        var inputYearStr = "";
 
-    var qs = "SELECT Year(`FILING DATE`) AS Year, QUARTER(`FILING DATE`) As Quarter,`PERMIT TYPE` As Permit_Type, P.FULLFORM As Permit_Desc, COUNT(*) As Count FROM `permit_history`,`permit_accronym` As P WHERE Year(`FILING DATE`)=" + inputYear +" AND `PERMIT TYPE` = P.ACCRONYM GROUP BY Year(`FILING DATE`),QUARTER(`FILING DATE`),`PERMIT TYPE`";
+    var qs = "SELECT "+
+                    "Year(`FILING DATE`) AS Year,"+
+                    "QUARTER(`FILING DATE`) As Quarter,"+
+                    "`PERMIT TYPE` As Permit_Type,"+
+                    "P.FULLFORM As Permit_Desc,"+
+                    "COUNT(*) As Count "+
+                "FROM `permit_history`,`permit_accronym` As P "+
+                "WHERE `PERMIT TYPE` = P.ACCRONYM "+inputYearStr+
+                "GROUP BY Year(`FILING DATE`),QUARTER(`FILING DATE`),`PERMIT TYPE`";
 	dbObject.find(qs/*condition, '*' , {}, 0, 0, {}*/, function(err, response){
 		if (err) {
         	//console.log("err", err);
