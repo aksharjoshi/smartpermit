474bde0bda853fca5e2050739025e0aa94a394b9
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-06 22:03:53 -0800

    Addind Year wise data selection Option

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -119,7 +119,6 @@ app.controller('analyticsController', function($scope,$http) {
 			});
 		});		
     };
-	$scope.getSeasonalData($scope.years[0]);
 	
 	$http.get("/popularPermit").success(function(response){
 		var permitsArray = [];
@@ -133,7 +132,7 @@ app.controller('analyticsController', function($scope,$http) {
 			}
 			permitsArray[obj.Permit_Type][parseInt(obj.Quarter)-1] = obj.permit_count;
 		});
-		console.log(permitsArray);
+	
 		$(permitTypes).each(function(idx,permit_type){
 			counts = [];
 			for(var i=0; i<4; i++){
@@ -141,7 +140,6 @@ app.controller('analyticsController', function($scope,$http) {
 			}
 			mostPopularPermitsArray.push({name: permit_desc[permit_type], data: counts})
 		});
-		console.log(mostPopularPermitsArray);
 		Highcharts.chart('containerMostPopularPermitsAnalytics', {
 
 	        chart: {
@@ -177,184 +175,191 @@ app.controller('analyticsController', function($scope,$http) {
 	    });
 	});
 
-	$http.get("/expirartionAnalysis?year=2012").success(function(response){
-		var expirationTrendArray = [];
-		var permitTypeCountArray = [];
-		var permitTypes = [];
-		var counts = [];
-		$(response).each(function(idx,obj){
-			if(typeof permitTypeCountArray[obj.Permit_Type] == "undefined" || typeof permitTypeCountArray[obj.Permit_Type] == null){
-				permitTypeCountArray[obj.Permit_Type] = {};
-				permitTypes.push(obj.Permit_Type);
-			}
-			permitTypeCountArray[obj.Permit_Type][parseInt(obj.Quarter)-1] = obj.Count;
-		});
-
-		$(permitTypes).each(function(idx,permit_type){
-			counts = [];
-			for(var i=0; i<4; i++){
-				counts.push(permitTypeCountArray[permit_type][i]);
-			}
-			expirationTrendArray.push({name: permit_desc[permit_type], data: counts})
-		});
-		
-		$(function () {
-		    Highcharts.chart('containerExpirationAnalytics', {
-		        title: {
-		            text: 'Permit Expiration Analysis',
-		            x: -20 //center
-		        },
-		        subtitle: {
-		            text: 'Source: New York Open Data',
-		            x: -20
-		        },
-		        xAxis: {
-		            categories: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']
-		        },
-		        yAxis: {
-		            title: {
-		                text: 'Total Number of Permits'
-		            },
-		        },
-		        legend: {
-		            layout: 'vertical',
-		            align: 'right',
-		            verticalAlign: 'middle',
-		            borderWidth: 0
-		        },
-		        series: expirationTrendArray
-		    });
-		});
-	});
 	
-
-	$http.get("/heatMap?year=2012").success(function(response){
-		console.log(response);
-		var zipObj = {};
-		var zipArray = [];
-		/*$(response).each(function(index,obj){
-			if(typeof zipObj[obj.zipcode] == "undefined" || typeof zipObj[obj.zipcode] == null){
-				zipObj[obj.zipcode] = {};
-				zipObj[obj.zipcode][obj.Permit_Type] = {}; 
-				zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
-			}
-			else{
-				if(obj.zipcode != "undefined" && obj.zipcode != null && obj.zipcode != ""){
-					if(typeof zipObj[obj.zipcode][obj.Permit_Type] == "undefined" || typeof zipObj[obj.Permit_Type][obj.Permit_Type] == null){
-						zipObj[obj.zipcode][obj.Permit_Type] = {}; 
-						zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
-					}
-					else{
-						zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
-					}	
+	$scope.getExpirartionAnalysisData = function(year) {
+		$http.get("/expirartionAnalysis?year="+year).success(function(response){
+			var expirationTrendArray = [];
+			var permitTypeCountArray = [];
+			var permitTypes = [];
+			var counts = [];
+			$(response).each(function(idx,obj){
+				if(typeof permitTypeCountArray[obj.Permit_Type] == "undefined" || typeof permitTypeCountArray[obj.Permit_Type] == null){
+					permitTypeCountArray[obj.Permit_Type] = {};
+					permitTypes.push(obj.Permit_Type);
 				}
-			}
-		});*/
-		$(response).each(function(index,obj){
-			if(typeof zipObj[obj.BOROUGH] == "undefined" || typeof zipObj[obj.BOROUGH] == null){
-				zipObj[obj.BOROUGH] = {};
-				zipObj[obj.BOROUGH][obj.zipcode] = {};
-				zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] = {}; 
-				zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
-			}
-			else{
-				if(obj.BOROUGH != "undefined" && obj.BOROUGH != null && obj.BOROUGH != ""){
-					if(typeof zipObj[obj.BOROUGH][obj.zipcode] == "undefined" || typeof zipObj[obj.BOROUGH][obj.zipcode] == null){
-						zipObj[obj.BOROUGH][obj.zipcode] = {};
-						zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] = {}; 
-						zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+				permitTypeCountArray[obj.Permit_Type][parseInt(obj.Quarter)-1] = obj.Count;
+			});
+
+			$(permitTypes).each(function(idx,permit_type){
+				counts = [];
+				for(var i=0; i<4; i++){
+					counts.push(permitTypeCountArray[permit_type][i]);
+				}
+				expirationTrendArray.push({name: permit_desc[permit_type], data: counts})
+			});
+			
+			$(function () {
+			    Highcharts.chart('containerExpirationAnalytics', {
+			        title: {
+			            text: 'Permit Expiration Analysis',
+			            x: -20 //center
+			        },
+			        subtitle: {
+			            text: 'Source: New York Open Data',
+			            x: -20
+			        },
+			        xAxis: {
+			            categories: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']
+			        },
+			        yAxis: {
+			            title: {
+			                text: 'Total Number of Permits'
+			            },
+			        },
+			        legend: {
+			            layout: 'vertical',
+			            align: 'right',
+			            verticalAlign: 'middle',
+			            borderWidth: 0
+			        },
+			        series: expirationTrendArray
+			    });
+			});
+		});
+	};
+
+	$scope.getHeatMapData = function(year) {
+		$http.get("/heatMap?year="+year).success(function(response){
+		
+			var zipObj = {};
+			var zipArray = [];
+			/*$(response).each(function(index,obj){
+				if(typeof zipObj[obj.zipcode] == "undefined" || typeof zipObj[obj.zipcode] == null){
+					zipObj[obj.zipcode] = {};
+					zipObj[obj.zipcode][obj.Permit_Type] = {}; 
+					zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+				}
+				else{
+					if(obj.zipcode != "undefined" && obj.zipcode != null && obj.zipcode != ""){
+						if(typeof zipObj[obj.zipcode][obj.Permit_Type] == "undefined" || typeof zipObj[obj.Permit_Type][obj.Permit_Type] == null){
+							zipObj[obj.zipcode][obj.Permit_Type] = {}; 
+							zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+						}
+						else{
+							zipObj[obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+						}	
 					}
-					else{
-						if(obj.zipcode != "undefined" && obj.zipcode != null && obj.zipcode != ""){
-							if(typeof zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] == "undefined" || typeof zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] == null){
-								zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] = {}; 
-								zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+				}
+			});*/
+			$(response).each(function(index,obj){
+				if(typeof zipObj[obj.BOROUGH] == "undefined" || typeof zipObj[obj.BOROUGH] == null){
+					zipObj[obj.BOROUGH] = {};
+					zipObj[obj.BOROUGH][obj.zipcode] = {};
+					zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] = {}; 
+					zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+				}
+				else{
+					if(obj.BOROUGH != "undefined" && obj.BOROUGH != null && obj.BOROUGH != ""){
+						if(typeof zipObj[obj.BOROUGH][obj.zipcode] == "undefined" || typeof zipObj[obj.BOROUGH][obj.zipcode] == null){
+							zipObj[obj.BOROUGH][obj.zipcode] = {};
+							zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] = {}; 
+							zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+						}
+						else{
+							if(obj.zipcode != "undefined" && obj.zipcode != null && obj.zipcode != ""){
+								if(typeof zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] == "undefined" || typeof zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] == null){
+									zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type] = {}; 
+									zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+								}
+								else{
+									zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
+								}	
 							}
-							else{
-								zipObj[obj.BOROUGH][obj.zipcode][obj.Permit_Type]["count"] = obj.permit_count;
-							}	
 						}
 					}
 				}
-			}
-		});
+			});
 		
-		var points = [],
-        regionP,
-        regionVal,
-        regionI = 0,
-        countryP,
-        countryI,
-        causeP,
-        causeI
-        var data2 = zipObj;
-   
-	    for (BOROUGH in data2) {
-	        if (data2.hasOwnProperty(BOROUGH)) {
-	            regionVal = 0;
-	            regionP = {
-	                id: 'id_' + regionI,
-	                name: BOROUGH,
-	                color: Highcharts.getOptions().colors[regionI]
-	            };
-	            countryI = 0;
-	            for (zipcode in data2[BOROUGH]) {
-	                if (data2[BOROUGH].hasOwnProperty(zipcode)) {
-	                    countryP = {
-	                        id: regionP.id + '_' + countryI,
-	                        name: zipcode,
-	                        parent: regionP.id
-	                    };
-	                    points.push(countryP);
-	                    causeI = 0;
-	                    for (permit in data2[BOROUGH][zipcode]) {
-	                        if (data2[BOROUGH][zipcode].hasOwnProperty(permit)) {
-	                            causeP = {
-	                                id: countryP.id + '_' + causeI,
-	                                name: permit_desc[permit],
-	                                parent: countryP.id,
-	                                value: Math.round(+data2[BOROUGH][zipcode][permit]["count"])
-	                            };
-	                            regionVal += causeP.value;
-	                            points.push(causeP);
-	                            causeI = causeI + 1;
-	                        }
-	                    }
-	                    countryI = countryI + 1;
-	                }
-	            }
-	            regionP.value = Math.round(regionVal);
-	            points.push(regionP);
-	            regionI = regionI + 1;
-	        }
-	    }
+			var points = [],
+	        regionP,
+	        regionVal,
+	        regionI = 0,
+	        countryP,
+	        countryI,
+	        causeP,
+	        causeI
+	        var data2 = zipObj;
+	   
+		    for(BOROUGH in data2){
+		        if (data2.hasOwnProperty(BOROUGH)) {
+		            regionVal = 0;
+		            regionP = {
+		                id: 'id_' + regionI,
+		                name: BOROUGH,
+		                color: Highcharts.getOptions().colors[regionI]
+		            };
+		            countryI = 0;
+		            for (zipcode in data2[BOROUGH]) {
+		                if (data2[BOROUGH].hasOwnProperty(zipcode)) {
+		                    countryP = {
+		                        id: regionP.id + '_' + countryI,
+		                        name: zipcode,
+		                        parent: regionP.id
+		                    };
+		                    points.push(countryP);
+		                    causeI = 0;
+		                    for (permit in data2[BOROUGH][zipcode]) {
+		                        if (data2[BOROUGH][zipcode].hasOwnProperty(permit)) {
+		                            causeP = {
+		                                id: countryP.id + '_' + causeI,
+		                                name: permit_desc[permit],
+		                                parent: countryP.id,
+		                                value: Math.round(+data2[BOROUGH][zipcode][permit]["count"])
+		                            };
+		                            regionVal += causeP.value;
+		                            points.push(causeP);
+		                            causeI = causeI + 1;
+		                        }
+		                    }
+		                    countryI = countryI + 1;
+		                }
+		            }
+		            regionP.value = Math.round(regionVal);
+		            points.push(regionP);
+		            regionI = regionI + 1;
+		        }
+		    }
     
-	    Highcharts.chart('containerlocationWiseAnalytics', {
-	        series: [{
-	            type: 'treemap',
-	            layoutAlgorithm: 'squarified',
-	            allowDrillToNode: true,
-	            animationLimit: 1000,
-	            dataLabels: {
-	                enabled: false
-	            },
-	            levelIsConstant: false,
-	            levels: [{
-	                level: 1,
-	                dataLabels: {
-	                    enabled: true
-	                },
-	                borderWidth: 3
-	            }],
-	            data: points
-	        }],
-	        subtitle: {
-	            text: 'Click points to drill down. Source: New York Open Data.'
-	        },
-	        title: {
-	            text: 'Area wise permits'
-	        }
-	    });
-	});
+		    Highcharts.chart('containerlocationWiseAnalytics', {
+		        series: [{
+		            type: 'treemap',
+		            layoutAlgorithm: 'squarified',
+		            allowDrillToNode: true,
+		            animationLimit: 1000,
+		            dataLabels: {
+		                enabled: false
+		            },
+		            levelIsConstant: false,
+		            levels: [{
+		                level: 1,
+		                dataLabels: {
+		                    enabled: true
+		                },
+		                borderWidth: 3
+		            }],
+		            data: points
+		        }],
+		        subtitle: {
+		            text: 'Click points to drill down. Source: New York Open Data.'
+		        },
+		        title: {
+		            text: 'Area wise permits'
+		        }
+		    });
+		});
+	};
 	
+	$scope.getSeasonalData($scope.years[0]);
+	$scope.getExpirartionAnalysisData($scope.years[0]);
+	$scope.getHeatMapData($scope.years[0]);
 });
