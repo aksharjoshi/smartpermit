4852b4fa8dd160146c1c9abc7f4fafa212d39159
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-10 11:21:34 -0800

    Adding Permit Heat Map - UI Updates

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -408,7 +408,7 @@ app.controller('analyticsController', function($scope,$http) {
         });
 
         heatmap = new google.maps.visualization.HeatmapLayer({
-          data: getPoints(),
+          data: $scope.getPoints(),
           map: map2
         });
 	};
