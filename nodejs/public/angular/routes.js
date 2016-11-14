f0f81ab2405beb7be2013857aecd96afbd4d7e6a
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-14 12:17:17 -0800

    Adding Permit Heat Map - UI Updates

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -421,17 +421,27 @@ app.controller('analyticsController', function($scope,$http) {
           		//fillOpacity: .2,
 			});
 
-        	var gradient = [					// rgba colors for the gradient
+        /*	var gradient = [					// rgba colors for the gradient
 	'rgba(255,255,0,0)','rgba(255,0,0,1)','rgba(255,0,0,1)','rgba(255,0,0,1)',
 	'rgba(255,0,0,1)','rgba(255,0,0,1)','rgba(255,0,0,1)','rgba(255,0,0,1)',
 	'rgba(255,0,0,1)','rgba(255,0,0,1)','rgba(255,0,0,1)','rgba(255,0,0,1)',
-	];
-	var gradient2 = [
-						'rgba(255, 0, 0, 0.2)',
-						'rgba(255, 0, 0, 0.4)',
-						'rgba(255, 0, 0, 0.6)',
-						'rgba(255, 0, 0, 0.8)',
-					];
+	];*/
+	var gradient = [
+    'rgba(0, 255, 255, 0)',
+    'rgba(0, 255, 255, 1)',
+    'rgba(0, 191, 255, 1)',
+    'rgba(0, 127, 255, 1)',
+    'rgba(0, 63, 255, 1)',
+    'rgba(0, 0, 255, 1)',
+    'rgba(0, 0, 223, 1)',
+    'rgba(0, 0, 191, 1)',
+    'rgba(0, 0, 159, 1)',
+    'rgba(0, 0, 127, 1)',
+    'rgba(63, 0, 91, 1)',
+    'rgba(127, 0, 63, 1)',
+    'rgba(191, 0, 31, 1)',
+    'rgba(255, 0, 0, 1)'
+  ]
 
 			heatmap.set('gradient', gradient);
 
