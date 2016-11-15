0798340357c0cf9280607cdb527d77f528fb71d3
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-14 16:32:05 -0800

    Adding Smart Questionnaire - UI

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -44,34 +44,12 @@ app.controller('homeController', function($scope,$http) {
 	
 });
 app.controller('permitsController', function($scope,$http) {
-	var form = $("#example-form");
-	form.validate({
-	    errorPlacement: function errorPlacement(error, element) { element.before(error); },
-	    rules: {
-	        confirm: {
-	            equalTo: "#password"
-	        }
-	    }
-	});
-	form.children("div").steps({
+	$("#example-basic").steps({
 	    headerTag: "h3",
 	    bodyTag: "section",
 	    transitionEffect: "slideLeft",
-	    onStepChanging: function (event, currentIndex, newIndex)
-	    {
-	        form.validate().settings.ignore = ":disabled,:hidden";
-	        return form.valid();
-	    },
-	    onFinishing: function (event, currentIndex)
-	    {
-	        form.validate().settings.ignore = ":disabled";
-	        return form.valid();
-	    },
-	    onFinished: function (event, currentIndex)
-	    {
-	        alert("Submitted!");
-	    }
-	});	
+	    autoFocus: true
+	});
 });
 
 app.controller('analyticsController', function($scope,$http) {
