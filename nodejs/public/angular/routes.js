d633ecf4fdc22880385621664e7839c97565fc40
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-17 00:01:25 -0800

    Adding Smart Questionnaire - Frontend

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -52,7 +52,7 @@ app.controller('permitsController', function($scope,$http) {
 	
 	$http.get("http://ec2-52-53-148-138.us-west-1.compute.amazonaws.com:3000/getquestion?id=1").success(function(response){
 	 	$scope.question = response.Question;
-	 	$scope.options = ((response.Next_question)).split(",");
+	 	$scope.options = $.parseJSON(response.Next_question);
 	 	console.log($.parseJSON(response.Next_question));
 	});
 
