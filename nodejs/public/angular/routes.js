7448a1b15c0b6c652327f612b4275a0f6dfe8d6c
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-23 16:24:46 -0800

    Drilldown analytics

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -217,6 +217,70 @@ app.controller('analyticsController', function($scope,$http) {
     };
     $scope.getDrillDownSeasonalPermits = function(quarter){
     	alert(quarter);
+    	$http.get("/seasonalAnalysis?year="+2012).success(function(response){
+			var seosonalTrendArray = [];
+			var permitTypeCountArray = [];
+			var permitTypes = [];
+			var counts = [];
+			
+			$(response).each(function(idx,obj){
+				if($.inArray(obj.Year, $scope.years) == -1)
+					$scope.years.push(obj.Year);
+				if(typeof permitTypeCountArray[obj.Permit_Type] == "undefined" || typeof permitTypeCountArray[obj.Permit_Type] == null){
+					permitTypeCountArray[obj.Permit_Type] = {};
+					permitTypes.push(obj.Permit_Type);
+				}
+				permitTypeCountArray[obj.Permit_Type][parseInt(obj.Quarter)-1] = obj.Count;
+			});
+
+			$(permitTypes).each(function(idx,permit_type){
+				counts = [];
+				for(var i=0; i<4; i++){
+					counts.push(permitTypeCountArray[permit_type][i]);
+				}
+				seosonalTrendArray.push({name: permit_desc[permit_type], data: counts})
+			});
+		
+			$(function () {
+			    Highcharts.chart('containerDrillDownSeasonalAnalytics', {
+			        title: {
+			            text: 'Seasonal Permit Trend',
+			            x: -20 //center
+			        },
+			        subtitle: {
+			            text: 'Source: New York Open Data',
+			            x: -20
+			        },
+			        xAxis: {
+			            categories: ['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4'],
+			            labels: {
+			                events: {
+			                    click: function (e) {
+			                        $scope.getDrillDownSeasonalPermits($(e.target).text());
+			                    }
+			                }
+			            }
+			        },
+			        yAxis: {
+			            title: {
+			                text: 'Total Number of Permits'
+			            },
+			            plotLines: [{
+			                value: 0,
+			                width: 1,
+			                color: '#808080'
+			            }]
+			        },
+			        legend: {
+			            layout: 'vertical',
+			            align: 'right',
+			            verticalAlign: 'middle',
+			            borderWidth: 0
+			        },
+			        series: seosonalTrendArray
+			    });
+			});
+		});	
     };
 
 	$scope.getPopularPermitsData = function(year) {
