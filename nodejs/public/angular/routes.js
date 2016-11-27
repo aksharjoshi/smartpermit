92cfec584ecb7327e6c77680618366d5691944d1
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-26 19:03:09 -0800

    Drilldown analytics

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -188,6 +188,7 @@ app.controller('analyticsController', function($scope,$http) {
 			            labels: {
 			                events: {
 			                    click: function (e) {
+			                    	e,preventDefault();
 			                        $scope.quarter = $(e.target).text();
 			                        $scope.getDrillDownSeasonalPermits($scope.quarter);
 			                    }
