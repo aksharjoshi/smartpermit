cb9eb393cb34fb9f514dac2e77a627127685d99d
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-27 19:12:56 -0800

    Recommendations UI

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -93,7 +93,7 @@ app.controller('recommendationController', function($scope,$http) {
 			getRecommendatinURL = getRecommendatinURL+"&permit_subtype="+permitSubtype;
 
 		$http.get(getRecommendatinURL).success(function(res){
-			console.log(res[0].ID);
+			$http.get("http://ec2-52-53-148-138.us-west-1.compute.amazonaws.com:8181/recommend?permitId="+res[0].ID+"&count=5").success(function(res){
 		});
 	};
 			
