f7fe2926e7375ff8374bf7a2b1ac405a6de9d8a7
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-28 22:13:05 -0800

    Adding glyphicons

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -246,7 +246,7 @@ app.controller('permitsController', function($scope,$http) {
 	 	$scope.options = $.parseJSON(response.Next_question);
 	 	$(".imgIcon").each(function(imgObj){
 	 		console.log(imgObj);
-	 		var icon = $(imgObj).attr("data-class");
+	 		var icon = $(imgObj).attr("data");
 	 		console.log($scope.icons[icon]);
 
 	 		imgObj.src = "/images/glyphicons_free/glyphicons/png/glyphicons-619-mixed-buildings.png";
