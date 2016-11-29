56e018d4e94aae33ea795e53f7c8183dc2954cc5
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-28 19:23:52 -0800

    Adding glyphicons

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -134,6 +134,8 @@ app.controller('permitsController', function($scope,$http) {
 	$scope.selectedOption = "";
 	$scope.showComponents = "no";
 
+	console.log($scope.icons);
+
 	$scope.next = function() {
 		if($scope.showComponents == "yes"){
 			$("#prePermitContainer").hide();
@@ -919,4 +921,27 @@ console.log(permitTypeCountArray);
 	$("#divLocationContent").find(".tab1:first").addClass("active");
 	$scope.initMap();
 	$scope.initHeatMap();
+
+	$scope.icons = {
+		"NEW" : "glyphicons-619-mixed-buildings",
+		"REMODELING" : "glyphicons-281-settings.png",
+		"COMMERCIAL" : "glyphicons-620-industrial-zone.png",
+		"RESIDENTIAL" : "glyphicons-21-home.png",
+		"OFFICE" : "glyphicons-90-building.png",
+		"RESIDENTIAL" : "glyphicons-21-home.png",
+		"OTHERS" : "",
+		"LIVINGROOM" : "glyphicons-87-display.png",
+		"HALL" : "glyphicons-87-display.png",
+		"BEDROOM" : "glyphicons-628-bedroom-lamp.png",
+		"KITCHEN" : "glyphicons-277-cutlery.png",
+		"BATHROOM" : "glyphicons-514-bath-bathtub.png",
+		"BASEMENT" : "glyphicons-681-door.png",
+		"STAIRS" : "glyphicons-431-construction-cone.png",
+		"BACKYARD" : "",
+		"PATIO" : "",
+		"CURB/FENCE" : "",
+		"OTHER" : "",
+		"YES" : "glyphicons-207-ok.png",
+		"NO" : "glyphicons-208-remove.png"
+	};
 });
