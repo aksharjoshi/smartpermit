a00bb172002cce865cc25d820e2c04937fa907cc
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-28 16:08:07 -0800

    Recommendation UI

--- nodejs/routes/recommendation.js
+++ nodejs/routes/recommendation.js
@@ -65,7 +65,7 @@ exports.getRecommendation = function(req, res){
         var options = {
           host: 'ec2-52-53-148-138.us-west-1.compute.amazonaws.com',
           port: 8181,
-          path: '/recommend?permitId=1&count=5',
+          path: '/recommend?permitId='+response[0].ID+'&count=5',
           method: 'GET'
         };
 
