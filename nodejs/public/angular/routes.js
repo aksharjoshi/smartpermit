c82cedf92289f303831c80464782f8d873f88483
dhvani.shah <shah.dhvani1091@gmail.com>
2016-11-28 19:32:57 -0800

    Adding glyphicons

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -208,6 +208,10 @@ app.controller('permitsController', function($scope,$http) {
 		 	$scope.questionID = prevQuestionid;
 		 	$scope.question = response.Question;
 		 	$scope.options = $.parseJSON(response.Next_question);
+		 	$($scope.options).each(function(k,opt){
+		 		console.log(opt);
+		 	});
+		 	//./images/glyphicons_free/glyphicons/png/{{}}
 		 	$scope.selectedOption = $scope.responses[$scope.questionID];
 		 	console.log("queston: "+$scope.questionID);
 		 	console.log($scope.selectedOption);
