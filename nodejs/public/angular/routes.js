2ef9fa8fd12f465b3e39e7cad53687d0e4747a8d
dhvani.shah <shah.dhvani1091@gmail.com>
2016-12-01 05:39:09 -0800

    Pre-permit - Multiple select

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -224,7 +224,10 @@ app.controller('permitsController', function($scope,$http) {
 					if(response.msg == "Success"){
 						$scope.showComponents = "no";
 						$scope.question = response.data.Question;
-						$scope.options = $.parseJSON(response.data.Next_question);
+						if((response.Next_question).indexOf("ANSWER") >= 0)
+							$scope.options = $.parseJSON(response.data.Options);
+						else
+							$scope.options = $.parseJSON(response.data.Next_question);
 						$scope.answer_type = response.data.Answer_type;
 						$scope.XQuestion.push(response.previous_answer);	
 						console.log($scope.XQuestion);	
