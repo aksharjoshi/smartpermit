54acb6263a83052aab8b6dde8bf4ce3150ab7166
dhvani.shah <shah.dhvani1091@gmail.com>
2016-12-01 03:09:49 -0800

    Pre-permit - Multiple select

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -245,6 +245,8 @@ app.controller('permitsController', function($scope,$http) {
 				 	if((response.Next_question).indexOf("ANSWER") >= 0){
 				 		var question = response.Question;
 				 		$($scope.XQuestion).each(function(idx,opt){
+				 			console.log(response.Next_question);
+				 			console.log(opt);
 				 			console.log(typeof response.Next_question[opt]);
 				 		});
 				 		
