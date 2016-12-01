22aa3fa3e6d99cec21a0f9338c4b4040cf3725c4
Akshar Joshi <akshar.joshi91@gmail.com>
2016-12-01 08:58:33 -0800

    VISUALIZATION UPDATES

--- nodejs/routes/visualization.js
+++ nodejs/routes/visualization.js
@@ -72,7 +72,7 @@ exports.expirartionAnalysis = function(req, res){
            // inputQtrGr = " AND MONTH(`FILING DATE`)";
         }
 
-        var qs = "SELECT Year(`EXPIRATION DATE`) AS Year, QUARTER(`EXPIRATION DATE`) As Quarter, Month(`FILING DATE`) As Month , `PERMIT TYPE` As Permit_Type, P.DESCRIPTION As Permit_Desc, COUNT(*) As Count FROM `permit_history`,`ACRONYM_MASTER` As P WHERE Year(`EXPIRATION DATE`)=" + inputYear + " AND `RESIDETIAL` = 'YES' AND `PERMIT TYPE` = P.ACRONYM GROUP BY Year(`EXPIRATION DATE`),QUARTER(`EXPIRATION DATE`),`PERMIT TYPE`";
+        var qs = "SELECT Year(`EXPIRATION DATE`) AS Year, QUARTER(`EXPIRATION DATE`) As Quarter, Month(`FILING DATE`) As Month , `PERMIT TYPE` As Permit_Type, P.DESCRIPTION As Permit_Desc, COUNT(*) As Count FROM `permit_history`,`ACRONYM_MASTER` As P WHERE Year(`EXPIRATION DATE`)=" + inputYear + " AND `RESIDETIAL` = 'YES' AND `PERMIT TYPE` = P.ACRONYM "+inputQtrStr+" GROUP BY Year(`EXPIRATION DATE`),QUARTER(`EXPIRATION DATE`),`PERMIT TYPE`";
 
        // var qs = "SELECT Year(`EXPIRATION_DATE`) AS Year, QUARTER(`EXPIRATION_DATE`) As Quarter,`PERMIT_TYPE` As Permit_Type, P.DESCRIPTION As Permit_Desc, COUNT(*) As Count FROM PERMIT_DETAILS,ACRONYM_MASTER As P WHERE `PERMIT_TYPE` = P.ACRONYM " + inputYearStr + " GROUP BY Year(`EXPIRATION_DATE`),QUARTER(`EXPIRATION_DATE`),PERMIT_TYPE";
 
