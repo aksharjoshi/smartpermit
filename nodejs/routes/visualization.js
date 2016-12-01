70d656c35967d12c1f52352b5a8c59522a4c5e4d
Akshar Joshi <akshar.joshi91@gmail.com>
2016-12-01 08:01:40 -0800

    VISUALIZATION UPDATES

--- nodejs/routes/visualization.js
+++ nodejs/routes/visualization.js
@@ -54,7 +54,7 @@ exports.expirartionAnalysis = function(req, res){
         else
             var inputYearStr = "";
 
-        var qs = "SELECT Year(`EXPIRATION DATE`) AS Year, QUARTER(`EXPIRATION DATE`) As Quarter,`PERMIT TYPE` As Permit_Type, P.DESCRIPTION As Permit_Desc, COUNT(*) As Count FROM `permit_history`,`ACRONYM_MASTER` As P WHERE Year(`EXPIRATION DATE`)=" + inputYear + " AND `RESIDETIAL` = 'YES' AND `PERMIT TYPE` = P.ACCRONYM GROUP BY Year(`EXPIRATION DATE`),QUARTER(`EXPIRATION DATE`),`PERMIT TYPE`";
+        var qs = "SELECT Year(`EXPIRATION DATE`) AS Year, QUARTER(`EXPIRATION DATE`) As Quarter,`PERMIT TYPE` As Permit_Type, P.DESCRIPTION As Permit_Desc, COUNT(*) As Count FROM `permit_history`,`ACRONYM_MASTER` As P WHERE Year(`EXPIRATION DATE`)=" + inputYear + " AND `RESIDETIAL` = 'YES' AND `PERMIT TYPE` = P.ACRONYM GROUP BY Year(`EXPIRATION DATE`),QUARTER(`EXPIRATION DATE`),`PERMIT TYPE`";
 
        // var qs = "SELECT Year(`EXPIRATION_DATE`) AS Year, QUARTER(`EXPIRATION_DATE`) As Quarter,`PERMIT_TYPE` As Permit_Type, P.DESCRIPTION As Permit_Desc, COUNT(*) As Count FROM PERMIT_DETAILS,ACRONYM_MASTER As P WHERE `PERMIT_TYPE` = P.ACRONYM " + inputYearStr + " GROUP BY Year(`EXPIRATION_DATE`),QUARTER(`EXPIRATION_DATE`),PERMIT_TYPE";
 
@@ -83,7 +83,7 @@ exports.heatMap = function(req, res){
 
        // var qs = "SELECT Year(`FILING DATE`) As Year, QUARTER(`FILING DATE`) As Quarter,`BOROUGH`,`ZIP CODE`,`PERMIT TYPE`,COUNT(*) FROM `permit_history` WHERE Year(`FILING DATE`)="+ inputYear + " AND `RESIDETIAL` = 'YES' GROUP BY Year(`FILING DATE`),QUARTER(`FILING DATE`),`BOROUGH`,`ZIP CODE`,`PERMIT TYPE`";
         var qs = "SELECT `BOROUGH`,`ZIP CODE` As zipcode,`PERMIT TYPE` As Permit_Type,Year(`FILING DATE`) AS Year,P.DESCRIPTION As Permit_Desc, COUNT(*) as permit_count FROM `permit_history`, `ACRONYM_MASTER` As P  WHERE `RESIDETIAL` = 'YES' "
-        +" AND `ZIP CODE` != '' AND `ZIP CODE` != 0 AND `PERMIT TYPE` = P.ACCRONYM AND Year(`FILING DATE`)="+inputYear+" GROUP BY `BOROUGH`,`ZIP CODE`,`PERMIT TYPE`,Year(`FILING DATE`)";
+        +" AND `ZIP CODE` != '' AND `ZIP CODE` != 0 AND `PERMIT TYPE` = P.ACRONYM AND Year(`FILING DATE`)="+inputYear+" GROUP BY `BOROUGH`,`ZIP CODE`,`PERMIT TYPE`,Year(`FILING DATE`)";
         
 
         /*var qs = "SELECT HOUSE_BOROUGH,HOUSE_ZIP As ZIPCODE,PERMIT_TYPE As Permit_Type,Year(`FILING_DATE`) AS Year,P.DESCRIPTION As Permit_Desc, COUNT(*) as permit_count FROM PERMIT_DETAILS, ACRONYM_MASTER As P  WHERE " +
@@ -120,7 +120,7 @@ exports.popularPermit = function(req, res){
                     " FROM `permit_history`, `ACRONYM_MASTER` As P"+
                     " WHERE `RESIDETIAL` = 'YES' "+
                     " AND Year(`FILING DATE`)="+inputYear+
-                    " AND `PERMIT TYPE` = P.ACCRONYM"+
+                    " AND `PERMIT TYPE` = P.ACRONYM"+
                     " GROUP BY `PERMIT TYPE`, QUARTER(`FILING DATE`) ";
         
         /*var qs = "SELECT Year(`FILING_DATE`) AS Year, `PERMIT_TYPE` As Permit_Type, QUARTER(`FILING_DATE`) As Quarter, P.DESCRIPTION As Permit_Desc, " +
