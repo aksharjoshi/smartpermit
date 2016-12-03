f7de10936bef28ab036f3321fd9eed222c3d994c
dhvani.shah <shah.dhvani1091@gmail.com>
2016-12-02 16:04:25 -0800

    Google maps - change color

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -1077,11 +1077,27 @@ console.log(permitTypeCountArray);
           		//fillOpacity: .2,
 			});
 
-        	var gradient = [					// rgba colors for the gradient
+        	var gradient2 = [					// rgba colors for the gradient
 	'rgba(255,255,0,0)','rgba(255,255,0,1)','rgba(255,191,255,1)','rgba(255,127,255,1)',
 	'rgba(255,63,255,1)','rgba(255,0,223,1)','rgba(255,0,191,1)','rgba(255,0,159,1)',
 	'rgba(255,0,127,1)','rgba(255,0,91,1)','rgba(255,0,31,1)','rgba(255,0,0,1)',
 	];
+	var gradient = [
+          'rgba(0, 255, 255, 0)',
+          'rgba(0, 255, 255, 1)',
+          'rgba(0, 191, 255, 1)',
+          'rgba(0, 127, 255, 1)',
+          'rgba(0, 63, 255, 1)',
+          'rgba(0, 0, 255, 1)',
+          'rgba(0, 0, 223, 1)',
+          'rgba(0, 0, 191, 1)',
+          'rgba(0, 0, 159, 1)',
+          'rgba(0, 0, 127, 1)',
+          'rgba(63, 0, 91, 1)',
+          'rgba(127, 0, 63, 1)',
+          'rgba(191, 0, 31, 1)',
+          'rgba(255, 0, 0, 1)'
+        ]
 
 	/*var gradient2 = [
 						'rgba(255, 0, 0, 0.2)',
