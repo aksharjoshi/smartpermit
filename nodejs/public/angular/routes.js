7b7b3d4cd26790417f9909352f5851105f1f1d75
dhvani.shah <shah.dhvani1091@gmail.com>
2016-12-02 18:00:45 -0800

    Adding Bubble maps

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -1266,10 +1266,10 @@ console.log(permitTypeCountArray);
         for (var city in citymap2) {
           // Add the circle for this city to the map.
           var cityCircle = new google.maps.Circle({
-            strokeColor: '#FF00FF',
+            strokeColor: '#000000',
             strokeOpacity: 0.8,
             strokeWeight: 2,
-            fillColor: '#FF0000',
+            fillColor: '#000000',
             fillOpacity: 0.35,
             map: map,
             center: citymap[city].center,
