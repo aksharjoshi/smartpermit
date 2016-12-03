3e32fc58f920e28a33ef624f2697459192eae9ce
dhvani.shah <shah.dhvani1091@gmail.com>
2016-12-02 17:52:18 -0800

    Adding Bubble maps

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -1201,6 +1201,50 @@ console.log(permitTypeCountArray);
           new google.maps.LatLng(37.751266, -122.403355)
         ];*/
       };
+
+     $scope.initBubbleMap = function(){
+     	var citymap = {
+        chicago: {
+          center: {lat: 41.878, lng: -87.629},
+          population: 2714856
+        },
+        newyork: {
+          center: {lat: 40.714, lng: -74.005},
+          population: 8405837
+        },
+        losangeles: {
+          center: {lat: 34.052, lng: -118.243},
+          population: 3857799
+        },
+        vancouver: {
+          center: {lat: 49.25, lng: -123.1},
+          population: 603502
+        }
+      };
+
+     	// Create the map.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 11,
+          center: {lat: 37.090, lng: -95.712},
+          mapTypeId: 'terrain'
+        });
+
+        // Construct the circle for each value in citymap.
+        // Note: We scale the area of the circle based on the population.
+        for (var city in citymap) {
+          // Add the circle for this city to the map.
+          var cityCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: citymap[city].center,
+            radius: Math.sqrt(citymap[city].population) * 100
+          });
+        }
+     };
 	
 	$scope.getSeasonalData($scope.years[0]);
 	$("#divSeasonalContent").find(".tab1:first").addClass("active");
@@ -1212,5 +1256,6 @@ console.log(permitTypeCountArray);
 	$("#divLocationContent").find(".tab1:first").addClass("active");
 	$scope.initMap();
 	$scope.initHeatMap();
+	$scope.initBubbleMap();
 
 });
