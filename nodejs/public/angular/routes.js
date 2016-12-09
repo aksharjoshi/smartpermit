fbfe02bbe246cf79e0acd6d5569fc1b0879a06e7
dhvani.shah <shah.dhvani1091@gmail.com>
2016-12-08 21:52:04 -0800

    Adding pdf

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -74,6 +74,9 @@ app.controller('homeController', function($scope,$http) {
 	    });*/
 	    doc.save('forms/common_pw1_form.pdf');
 	});
+	$scope.downloadForm = function(e){
+		e.preventDefault()
+	};
 		
 });
 
