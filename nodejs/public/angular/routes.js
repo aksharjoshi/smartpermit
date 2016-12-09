5c57b655db46d64dac632ac2abcf43fa893e10f9
dhvani.shah <shah.dhvani1091@gmail.com>
2016-12-09 03:57:55 -0800

    Updating pre-permit UI

--- nodejs/public/angular/routes.js
+++ nodejs/public/angular/routes.js
@@ -279,7 +279,7 @@ app.controller('permitsController', function($scope,$http) {
 							//$('.slider1').html(html);
 							setTimeout(function(){ 
 						 		$('.slider1').bxSlider({
-								    slideWidth: 5000,
+								    slideWidth: 1000,
 								    minSlides: 2,
 								    maxSlides: 3,
 								    slideMargin: 10
